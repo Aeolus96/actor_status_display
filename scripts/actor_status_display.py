adc2eeed8be624b3895a216c638e98841a1fe8d0
Devson Butani <dbutani@ltu.edu>
2024-05-22 14:38:57 -0400

    Updated to recieve filename

--- scripts/actor_status_display.py
+++ scripts/actor_status_display.py
@@ -1,48 +1,48 @@
 #!/usr/bin/env python3
 
-import rospy
-from sensor_msgs.msg import Image
-import cv2  # Needed on arm64 systems because cv_bridge is commonly amd64
-from cv_bridge import CvBridge
 import os
+
+import cv2  # Needed on arm64 systems because cv_bridge is commonly amd64
 import rospkg
+import rospy
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
 
 
-def publish_image():
-    rospy.init_node("actor_status_display_node", anonymous=True)
-    image_pub = rospy.Publisher("wled_bridge/image", Image, queue_size=1)
-    rate = rospy.Rate(0.1)  # Adjust the publishing Hz rate as needed
+class ImagePublisher:
+    def __init__(self):
+        rospy.init_node("actor_status_display_node", anonymous=True)
+        self.image_pub = rospy.Publisher("wled_bridge/image", Image, queue_size=1)
+        rospy.Subscriber("led_panel_image_filename", String, self.filename_callback)
+        self.bridge = CvBridge()
 
-    bridge = CvBridge()
-
-    while not rospy.is_shutdown():
-        # Load a sample PNG image
-        image_path = rospkg.RosPack().get_path("actor_status_display") + "/images/E_Stop.png"
+    def filename_callback(self, msg):
+        filename = msg.data
+        image_path = rospkg.RosPack().get_path("actor_status_display") + f"/images/{filename}.png"
 
         if os.path.exists(image_path):
             # Read the image
             cv_image = cv2.imread(image_path)
 
-            # Show the image
-            #cv2.imshow("Image", cv_image)
-            #cv2.waitKey(0)
-            #cv2.destroyAllWindows()
-
             # Convert the image to ROS Image message
-            ros_image_msg = bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
+            ros_image_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding="bgr8")
 
             # Publish the ROS Image message
-            image_pub.publish(ros_image_msg)
+            self.image_pub.publish(ros_image_msg)
 
-            rospy.loginfo("Image published to wled_bridge/image topic")
+            rospy.loginfo(f"Image '{filename}' published to wled_bridge/image topic")
+        else:
+            rospy.logwarn(f"Image file '{image_path}' does not exist")
 
-        rate.sleep()
+    def run(self):
+        rospy.spin()
 
 
 if __name__ == "__main__":
     try:
-        publish_image()
+        image_publisher = ImagePublisher()
+        image_publisher.run()
     except rospy.ROSInterruptException:
         cv2.destroyAllWindows()
         pass
-
